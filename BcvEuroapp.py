faed7e520116f6682dda66ed4e69282f53de87e0
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-03-27 18:03:11 -0400

    Update BcvEuroapp.py

--- BcvEuroapp.py
+++ BcvEuroapp.py
@@ -1,7 +1,26 @@
 import requests
 import json
+import os
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+# Configuración de Firebase
+service_account_info = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT'))
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred)
+def enviar_notificacion_precio(nuevo_precio):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title='¡El BCV Actualizó! 🚀',
+            body=f'El nuevo precio oficial es: {nuevo_precio} Bs.',
+        ),
+        topic='bcv_updates',
+    )
+    messaging.send(message)
+    print("Notificación enviada a los usuarios.")
 
 def capturar():
+    # ... aquí sigue el resto de tu código ...
     
     # Esta es la URL más estable ahorita para el BCV
     url = "https://ve.dolarapi.com/v1/dolares/oficial"
@@ -29,7 +48,10 @@ def capturar():
         with open("Bcveuro.json", "w") as f:
             json.dump(resultado, f, indent=4)
         print("¡LOGRADO!")
-            
+          # Buscamos el precio que acabas de obtener
+        precio_bcv = data.get('promedio')
+        # ¡Mandamos la notificación!
+        enviar_notificacion_precio(precio_bcv)  
     except Exception as e:
         print(f"Error: {e}")
 
